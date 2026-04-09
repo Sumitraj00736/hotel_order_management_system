1c0a266a7b393eb195e862fa169f423dfad5b8f6
Sumit Raj <sumitraj00736@gmail.com>
2026-04-09 18:11:21 +0545

    feat: customer record setup in backend

---
+++ src/routes/customerRoutes.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const { body, param } = require('express-validator');
+const auth = require('../middleware/auth');
+const branchScope = require('../middleware/branchScope');
+const requirePermission = require('../middleware/requirePermission');
+const validate = require('../middleware/validate');
+const {
+  listCustomers,
+  createCustomer,
+  updateCustomer,
+  deleteCustomer,
+  getRewardsSettings,
+  updateRewardsSettings
+} = require('../controllers/customerController');
+
+const router = express.Router();
+router.use(auth, branchScope);
+
+router.get('/', requirePermission('customers:view'), listCustomers);
+router.post(
+  '/',
+  requirePermission('customers:edit'),
+  [body('name').notEmpty().withMessage('Customer name is required')],
+  validate,
+  createCustomer
+);
+router.put(
+  '/:id',
+  requirePermission('customers:edit'),
+  [param('id').isMongoId()],
+  validate,
+  updateCustomer
+);
+router.delete(
+  '/:id',
+  requirePermission('customers:edit'),
+  [param('id').isMongoId()],
+  validate,
+  deleteCustomer
+);
+router.get('/rewards', requirePermission('customers:view'), getRewardsSettings);
+router.put('/rewards', requirePermission('customers:edit'), updateRewardsSettings);
+
+module.exports = router;
