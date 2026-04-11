da996e1e7833148773ed4122983471cf277b06fc
Sumit Raj <sumitraj00736@gmail.com>
2026-04-11 20:09:49 +0545

    feat: add logic to drop legacy indexes and sync Table collection on server startup

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const { Server } = require('socket.io');
 const app = require('./app');
 const { attachSocket } = require('./utils/socket');
+const Table = require('./models/Table');
 
 const PORT = process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI;
@@ -28,8 +29,18 @@ attachSocket(io);
 
 mongoose
   .connect(MONGO_URI, { serverSelectionTimeoutMS: 5000 })
-  .then(() => {
+  .then(async () => {
     console.log('MongoDB connected');
+    try {
+      const indexes = await Table.collection.indexes();
+      const legacyIndex = indexes.find((idx) => idx.name === 'tableNumber_1');
+      if (legacyIndex) {
+        await Table.collection.dropIndex('tableNumber_1');
+      }
+      await Table.syncIndexes();
+    } catch (error) {
+      console.warn('Table index sync skipped:', error.message);
+    }
     server.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
